e6a59c7ba8e7dcd51e266cb3f087e432256b29ce
jack-higi <jmiller@higi.com>
2015-01-09 15:32:34 -0600

    initial commit

---
+++ higi/ChallengeDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ChallengeDetailsViewController.swift
+//  higi
+//
+//  Created by Jack Miller on 1/9/15.
+//  Copyright (c) 2015 higi, LLC. All rights reserved.
+//
+
+import Foundation
+
+class ChallengeDetailsViewController: BaseViewController {
+    @IBOutlet var pointsLabel:UILabel;
+}
