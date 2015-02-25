abf3e338271f8af9b206cf54e2ca781ccf244e0e
jack-higi <jmiller@higi.com>
2015-02-25 13:16:06 -0600

    HMI-352: resizing progress legend row fixed

--- higi/ChallengeDetailsViewController.swift
+++ higi/ChallengeDetailsViewController.swift
@@ -1066,11 +1066,10 @@ class ChallengeDetailsViewController: UIViewController, UIScrollViewDelegate, UI
     
     func createProgressLegendRow(index: Int) -> UITableViewCell {
         let winConditions = isIndividualProgress ? individualGoalWinConditions : teamGoalWinConditions;
-        let points = isIndividualProgress ? challenge.participant.units: challenge.participant.team.units;
+        let points = isIndividualProgress ? challenge.participant.units : challenge.participant.team.units;
         let cell = ChallengeProgressLegendRow.instanceFromNib(winConditions[winConditions.count - index - 1], userPoints: points,  metric: challenge.abbrMetric, index: index + 1);
         cell.prizeDescription.sizeToFit();
         return cell;
-        
     }
     
     func getProgressLegendRowHeight(index:Int) -> CGFloat {
