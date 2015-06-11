dff79ac856cd31f6b747d46461231fd897a3cb09
jack-higi <jmiller@higi.com>
2015-06-11 17:17:48 -0500

    fixed panel header

--- higi/MetricDetailCard.swift
+++ higi/MetricDetailCard.swift
@@ -110,7 +110,7 @@ class MetricDetailCard: UIView {
     func secondPanelClicked(sender: AnyObject) {
         (Utility.getViewController(self) as! MetricsViewController).openDetailsIfClosed();
         if (thirdPanelSelected) {
-            triangleIndicator.frame = CGRect(x: secondPanel.frame.origin.x + secondPanel.frame.size.width / 2, y: secondPanel.frame.size.height - 2, width: triangleHeight, height: triangleHeight);
+            triangleIndicator.frame = CGRect(x: secondPanel.frame.origin.x + secondPanel.frame.size.width / 2 - triangleHeight / 2, y: secondPanel.frame.size.height - 2, width: triangleHeight, height: triangleHeight);
 //            gauge = MetricGauge.create(CGRect(x: 0, y: 0, width: gaugeContainer.frame.size.width, height: gaugeContainer.frame.size.height), delegate: delegate, userValue: value);
         }
         thirdPanelSelected = false;
@@ -119,7 +119,7 @@ class MetricDetailCard: UIView {
     func thirdPanelClicked(sender: AnyObject) {
         (Utility.getViewController(self) as! MetricsViewController).openDetailsIfClosed();
         if (!thirdPanelSelected) {
-            triangleIndicator.frame = CGRect(x: thirdPanel.frame.origin.x + thirdPanel.frame.size.width / 2, y: thirdPanel.frame.size.height - 2, width: triangleHeight, height: triangleHeight);
+            triangleIndicator.frame = CGRect(x: thirdPanel.frame.origin.x + thirdPanel.frame.size.width / 2 - triangleHeight / 2, y: thirdPanel.frame.size.height - 2, width: triangleHeight, height: triangleHeight);
 //            gauge = MetricGauge.create(CGRect(x: 0, y: 0, width: gaugeContainer.frame.size.width, height: gaugeContainer.frame.size.height), delegate: delegate, userValue: value);
         }
         thirdPanelSelected = true;
