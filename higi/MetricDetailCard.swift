28291e12892614da300ec47cefed54e9821c6064
jack-higi <jmiller@higi.com>
2015-07-10 17:06:28 -0500

    HMI-503: details card fits 4s

--- higi/MetricDetailCard.swift
+++ higi/MetricDetailCard.swift
@@ -68,7 +68,9 @@ class MetricDetailCard: UIView {
             if (gauge != nil && gauge.superview != nil) {
                 gauge.removeFromSuperview();
             }
-            meter = PointsMeter.create(CGRect(x: 0, y: 0, width: gaugeContainer.frame.size.width - 50, height: gaugeContainer.frame.size.height - 50), thickArc: true);
+            let meterWidth = gaugeContainer.frame.size.width - 50;
+            let meterHeight = gaugeContainer.frame.size.height;
+            meter = PointsMeter.create(CGRect(x: (gaugeContainer.frame.size.width - meterWidth) / 2, y: (gaugeContainer.frame.size.height - meterHeight) / 2, width: meterWidth, height: meterHeight), thickArc: true);
             meter.setLightArc();
             let dateString = Constants.dateFormatter.stringFromDate(Constants.displayDateFormatter.dateFromString(delegate.getSelectedPoint()!.date)!);
             if (SessionController.Instance.activities[dateString] != nil) {
