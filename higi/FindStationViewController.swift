affc7e508f192e44557ee783d83c85bd41c5d0eb
Dan Harms <dharms@higi.com>
2015-08-06 15:19:25 -0500

    Point to new cdn for retailer icons

--- higi/FindStationViewController.swift
+++ higi/FindStationViewController.swift
@@ -550,7 +550,7 @@ class FindStationViewController: BaseViewController, GMSMapViewDelegate, UITable
     func getKioskLogoUrl(kiosk: KioskInfo!) -> NSURL {
         var modifiedName = kiosk.organizations[0];
         modifiedName = modifiedName.stringByReplacingOccurrencesOfString(" ", withString: "_").stringByReplacingOccurrencesOfString("'", withString: "").stringByReplacingOccurrencesOfString("&", withString: "");
-        return NSURL(string: "https://webqa.superbuddytime.com/images/retailer-icons/\(modifiedName)_100.png")!;
+        return NSURL(string: "http://az646341.vo.msecnd.net/retailer-icons/\(modifiedName)_100.png")!;
     }
     
     @IBAction func startReminder(sender: AnyObject) {
