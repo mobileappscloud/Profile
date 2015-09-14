3b0092183cc1e0dd57e8edad8b6ede982ec983b5
Remy Panicker <rpanicker@higi.com>
2015-09-14 14:18:06 -0500

    HMI-619: Set Crashlytics debug mode

--- higi/AppDelegate.swift
+++ higi/AppDelegate.swift
@@ -21,6 +21,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
+        #if DEBUG
+            Crashlytics.sharedInstance().debugMode = true;
+        #endif
         Fabric.with([Crashlytics.self()])
 
         // Override point for customization after application launch.
