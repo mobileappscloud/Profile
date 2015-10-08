94d839da7e5bf5bc8d545ad12c76a893d5125028
Remy Panicker <rpanicker@higi.com>
2015-10-08 10:11:08 -0500

    HMI-626: l10n wip

--- higi/BpMetricDelegate.swift
+++ higi/BpMetricDelegate.swift
@@ -3,11 +3,16 @@ import Foundation
 class BpMetricDelegate: MetricDelegate {
     
     var selectedCheckin: HigiCheckin!;
-
+    
     struct BpRanges {
-        static let systolicRanges:[MetricGauge.Range] = [MetricGauge.Range(label: "Normal", color: Utility.colorFromHexString("#88c681"), interval: (90, 120)), MetricGauge.Range(label: "At risk", color: Utility.colorFromHexString("#fdd835"), interval: (120, 140)), MetricGauge.Range(label: "High", color: Utility.colorFromHexString("#ef535a"), interval: (140, 200))];
+        static let systolicRanges:[MetricGauge.Range] =
+        [MetricGauge.Range(label: NSLocalizedString("BLOOD_PRESSURE_RANGE_NORMAL_TITLE", comment: "Title for blood pressure within a normal range."), color: Utility.colorFromHexString("#88c681"), interval: (90, 120)),
+            MetricGauge.Range(label: NSLocalizedString("BLOOD_PRESSURE_RANGE_AT_RISK_TITLE", comment: "Title for blood pressure within an at-risk range."), color: Utility.colorFromHexString("#fdd835"), interval: (120, 140)),
+            MetricGauge.Range(label: NSLocalizedString("BLOOD_PRESSURE_RANGE_HIGH_TITLE", comment: "Title for blood pressure within a high range."), color: Utility.colorFromHexString("#ef535a"), interval: (140, 200))];
         
-        static let diastolicRanges:[MetricGauge.Range] = [MetricGauge.Range(label: "Normal", color: Utility.colorFromHexString("#88c681"), interval: (60, 80)), MetricGauge.Range(label: "At risk", color: Utility.colorFromHexString("#fdd835"), interval: (80, 90)), MetricGauge.Range(label: "High", color: Utility.colorFromHexString("#ef535a"), interval: (90, 120))];
+        static let diastolicRanges:[MetricGauge.Range] =
+        [MetricGauge.Range(label: NSLocalizedString("BLOOD_PRESSURE_RANGE_NORMAL_TITLE", comment: "Title for blood pressure within a normal range."), color: Utility.colorFromHexString("#88c681"), interval: (60, 80)), MetricGauge.Range(label: NSLocalizedString("BLOOD_PRESSURE_RANGE_AT_RISK_TITLE", comment: "Title for blood pressure within an at-risk range."), color: Utility.colorFromHexString("#fdd835"), interval: (80, 90)),
+            MetricGauge.Range(label: NSLocalizedString("BLOOD_PRESSURE_RANGE_HIGH_TITLE", comment: "Title for blood pressure within a high range."), color: Utility.colorFromHexString("#ef535a"), interval: (90, 120))];
     }
     
     func getTitle() -> String {
@@ -43,7 +48,7 @@ class BpMetricDelegate: MetricDelegate {
     }
     
     func getBlankStateText() -> String {
-        return "Welcome! It looks like you don’t have any blood pressure readings with us. Join the millions who track their BP with higi by visiting a higi Station or connecting your wireless BP monitor.";
+        return NSLocalizedString("BLOOD_PRESSURE_METRICS_VIEW_BLANK_STATE_TEXT", comment: "Text to display on blood pressure metrics view when there is no blood pressure data to display.");
     }
     
     func setSelected(date: NSDate) {
@@ -65,24 +70,25 @@ class BpMetricDelegate: MetricDelegate {
         if (selectedCheckin == nil) {
             return nil;
         } else {
+            // TODO: l10n formats
             let date = Constants.dateFormatter.stringFromDate(selectedCheckin.dateTime);
             let bp = selectedCheckin.systolic != nil ? "\(selectedCheckin.systolic!)/\(selectedCheckin.diastolic!)" : "";
             let map = selectedCheckin.map != nil ? String(format: "%.1f", arguments: [selectedCheckin.map!]) : "";
             var device = "";
             if let kioskInfo = selectedCheckin.kioskInfo {
-                return SelectedPoint(date: date, firstPanelValue: bp, firstPanelLabel: "Blood Pressure", firstPanelUnit: "mmHg", secondPanelValue: map, secondPanelLabel: "Mean Arterial Pressure", secondPanelUnit: "mmHg", kioskInfo: kioskInfo);
+                return SelectedPoint(date: date, firstPanelValue: bp, firstPanelLabel: NSLocalizedString("BLOOD_PRESSURE_METRICS_VIEW_SELECTED_POINT_PANEL_BLOOD_PRESSURE_LABEL", comment: "Label to display for a selected blood pressure data point."), firstPanelUnit: NSLocalizedString("GENERAL_PURPOSE_UNIT_LABEL_ABBR_MILLIMETERS_OF_MERCURY", comment: "General purpose abbreviated label for the units of millimeter of mercury."), secondPanelValue: map, secondPanelLabel: "Mean Arterial Pressure", secondPanelUnit: NSLocalizedString("GENERAL_PURPOSE_UNIT_LABEL_ABBR_MILLIMETERS_OF_MERCURY", comment: "General purpose abbreviated label for the units of millimeter of mercury."), kioskInfo: kioskInfo);
             } else if let vendorId = selectedCheckin.sourceVendorId {
                 device = "\(vendorId)";
             }
-            return SelectedPoint(date: date, firstPanelValue: bp, firstPanelLabel: "Blood Pressure", firstPanelUnit: "mmHg", secondPanelValue: map, secondPanelLabel: "Mean Arterial Pressure", secondPanelUnit: "mmHg", device: device);
+            return SelectedPoint(date: date, firstPanelValue: bp, firstPanelLabel: NSLocalizedString("BLOOD_PRESSURE_METRICS_VIEW_SELECTED_POINT_PANEL_BLOOD_PRESSURE_LABEL", comment: "Label to display for a selected blood pressure data point."), firstPanelUnit: NSLocalizedString("GENERAL_PURPOSE_UNIT_LABEL_ABBR_MILLIMETERS_OF_MERCURY", comment: "General purpose abbreviated label for the units of millimeter of mercury."), secondPanelValue: map, secondPanelLabel: NSLocalizedString("BLOOD_PRESSURE_METRICS_VIEW_SELECTED_POINT_PANEL_MEAN_ARTERIAL_PRESSURE_LABEL", comment: "Label to display for a selected mean arterial pressure data point."), secondPanelUnit: NSLocalizedString("GENERAL_PURPOSE_UNIT_LABEL_ABBR_MILLIMETERS_OF_MERCURY", comment: "General purpose abbreviated label for the units of millimeter of mercury."), device: device);
         }
     }
 
     func getRanges() -> [MetricGauge.Range] {
         var ranges:[MetricGauge.Range] = [];
-        ranges.append(MetricGauge.Range(label: "At risk", color: Utility.colorFromHexString("#fdd835"), interval: (30, 70)));
-        ranges.append(MetricGauge.Range(label: "Normal", color: Utility.colorFromHexString("#88c681"), interval: (70, 110)));
-        ranges.append(MetricGauge.Range(label: "High", color: Utility.colorFromHexString("#ef535a"), interval: (110, 150)));
+        ranges.append(MetricGauge.Range(label: NSLocalizedString("BLOOD_PRESSURE_RANGE_AT_RISK_TITLE", comment: "Title for blood pressure within an at-risk range."), color: Utility.colorFromHexString("#fdd835"), interval: (30, 70)));
+        ranges.append(MetricGauge.Range(label: NSLocalizedString("BLOOD_PRESSURE_RANGE_NORMAL_TITLE", comment: "Title for blood pressure within a normal range."), color: Utility.colorFromHexString("#88c681"), interval: (70, 110)));
+        ranges.append(MetricGauge.Range(label: NSLocalizedString("BLOOD_PRESSURE_RANGE_HIGH_TITLE", comment: "Title for blood pressure within a high range."), color: Utility.colorFromHexString("#ef535a"), interval: (110, 150)));
         return ranges;
     }
     
@@ -110,9 +116,9 @@ class BpMetricDelegate: MetricDelegate {
                 }
             }
         } else if (tab == 1) {
-            ranges.append(MetricGauge.Range(label: "Low", color: Utility.colorFromHexString("#44aad8"), interval: (30, 70)));
-            ranges.append(MetricGauge.Range(label: "Normal", color: Utility.colorFromHexString("#88c681"), interval: (70, 110)));
-            ranges.append(MetricGauge.Range(label: "High", color: Utility.colorFromHexString("#ef535a"), interval: (110, 150)));
+            ranges.append(MetricGauge.Range(label: NSLocalizedString("BLOOD_PRESSURE_RANGE_LOW_TITLE", comment: "Title for blood pressure within a low range."), color: Utility.colorFromHexString("#44aad8"), interval: (30, 70)));
+            ranges.append(MetricGauge.Range(label: NSLocalizedString("BLOOD_PRESSURE_RANGE_NORMAL_TITLE", comment: "Title for blood pressure within a normal range."), color: Utility.colorFromHexString("#88c681"), interval: (70, 110)));
+            ranges.append(MetricGauge.Range(label: NSLocalizedString("BLOOD_PRESSURE_RANGE_HIGH_TITLE", comment: "Title for blood pressure within a high range."), color: Utility.colorFromHexString("#ef535a"), interval: (110, 150)));
         }
         return ranges;
     }
@@ -129,7 +135,7 @@ class BpMetricDelegate: MetricDelegate {
     }
     
     func getSelectedUnit(tab: Int) -> String {
-        return "mmHg";
+        return NSLocalizedString("GENERAL_PURPOSE_UNIT_LABEL_ABBR_MILLIMETERS_OF_MERCURY", comment: "General purpose abbreviated label for the units of millimeter of mercury.");
     }
     
     func getSelectedClass(tab: Int) -> String {
@@ -145,6 +151,7 @@ class BpMetricDelegate: MetricDelegate {
         return "";
     }
     
+    // TODO: Verify this isn't grabbing localized strings....
     func colorFromClass(className: String, tab: Int) -> UIColor {
         var color: UIColor;
         if tab == 0 {
