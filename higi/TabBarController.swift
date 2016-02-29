7a18fcb8ba4f189857a9ee217135aabc47c87de4
Remy Panicker <rpanicker@higi.com>
2016-02-29 13:24:17 -0600

    HC-54: Refactory, localize tab bar items

--- higi/TabBarController.swift
+++ higi/TabBarController.swift
@@ -10,43 +10,67 @@ import UIKit
 
 final class TabBarController: UITabBarController {
 
-    private(set) lazy var homeViewController: DashboardViewController = {
-        return DashboardViewController(nibName: "DashboardView", bundle: nil)
+    lazy private(set) var homeViewController: DashboardViewController = {
+        let homeViewController = DashboardViewController(nibName: "DashboardView", bundle: nil)
+        return homeViewController
     }()
     
-    private(set) lazy var challengesViewController: ChallengesViewController = {
+    lazy private(set) var challengesViewController: ChallengesViewController = {
         return ChallengesViewController(nibName: "ChallengesView", bundle: nil)
     }()
     
-    private(set) lazy var metricsViewController: NewMetricsViewController = {
+    lazy private(set) var metricsViewController: NewMetricsViewController = {
         let navController = UIStoryboard(name: "Metrics", bundle: nil).instantiateInitialViewController() as! UINavigationController
         return navController.topViewController as! NewMetricsViewController
     }()
     
-    private(set) lazy var findStationViewController: FindStationViewController = {
+    lazy private(set) var findStationViewController: FindStationViewController = {
        return FindStationViewController(nibName: "FindStationView", bundle: nil)
     }()
     
-    // MARK: View Lifecycle
+    // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let homeNav = UINavigationController(rootViewController: homeViewController)
-        homeNav.tabBarItem = UITabBarItem(title: "Dashboard", image: UIImage(named: "home-tab-bar-icon"), selectedImage: UIImage(named: "home-tab-bar-highlight-icon"))
-        
-        let challengeNav = UINavigationController(rootViewController: challengesViewController)
-        challengeNav.tabBarItem = UITabBarItem(title: "Challenges", image: UIImage(named: "challenges-tab-bar-icon"), selectedImage: UIImage(named: "challenges-tab-bar-highlight-icon"))
-        
-        let metricsNav = metricsViewController.navigationController!
-        metricsNav.tabBarItem = UITabBarItem(title: "Metrics", image: UIImage(named: "metrics-tab-bar-icon"), selectedImage: UIImage(named: "metrics-tab-bar-highlight-icon"))
-        
-        let findStationNav = UINavigationController(rootViewController: findStationViewController)
-        findStationNav.tabBarItem = UITabBarItem(title: "Find Station", image: UIImage(named: "station-tab-bar-icon"), selectedImage: UIImage(named: "station-tab-bar-highlight-icon"))
-        
-        self.viewControllers = [homeNav, challengeNav, metricsNav, findStationNav]
-        
+        addViewControllers()
         self.tabBar.tintColor = Theme.Color.primary
+    }
+}
+
+// MARK: - Configuration
+
+extension TabBarController {
+    
+    private func addViewControllers() {
+        
+        let dashboardTitle = NSLocalizedString("MAIN_TAB_BAR_ITEM_TITLE_HOME", comment: "Title for Home tab bar item.")
+        let challengesTitle = NSLocalizedString("MAIN_TAB_BAR_ITEM_TITLE_CHALLENGES", comment: "Title for Challenges tab bar item.")
+        let metricsTitle = NSLocalizedString("MAIN_TAB_BAR_ITEM_TITLE_METRICS", comment: "Title for Metrics tab bar item.")
+        let findStationsTitle = NSLocalizedString("MAIN_TAB_BAR_ITEM_TITLE_FIND_STATION", comment: "Title for Find Station tab bar item.")
+        
+        typealias tabConfigItem = (viewController: UIViewController, navController: UINavigationController?, imageNamePrefix: String, title: String)
+        let items: [tabConfigItem] = [(homeViewController, nil, "home", dashboardTitle),
+                                      (challengesViewController, nil, "challenges", challengesTitle),
+                                      (metricsViewController, metricsViewController.navigationController, "metrics", metricsTitle),
+                                      (findStationViewController, nil, "station", findStationsTitle)
+        ]
+        
+        var configuredControllers: [UIViewController] = []
+        for var item in items {
+            item.viewController.navigationItem.rightBarButtonItem = navigationOverflowBarButtonItem()
+            if item.navController == nil {
+                item.navController = UINavigationController(rootViewController: item.viewController)
+            }
+            guard let navController = item.navController else { continue }
+            
+            let image = UIImage(named: "\(item.imageNamePrefix)-tab-bar-icon")
+            let highlightImage = UIImage(named: "\(item.imageNamePrefix)-tab-bar-highlight-icon")
+            navController.tabBarItem = UITabBarItem(title: item.title, image: image, selectedImage: highlightImage)
+            configuredControllers.append(navController)
+        }
+        
+        self.viewControllers = configuredControllers
     }
 }
 
