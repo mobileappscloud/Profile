4a7c13ad99ff774ecbc639b7e71f9bc865276cef
Remy Panicker <rpanicker@higi.com>
2016-03-15 17:35:05 -0500

    HMI-864: Refactor, prevent NaN is there are no points

--- higi/CPTXYPlotSpace+Utility.swift
+++ higi/CPTXYPlotSpace+Utility.swift
@@ -9,38 +9,37 @@
 extension CPTXYPlotSpace {
     
     func configure(points: [GraphPoint], maxY: Double, minY: Double, delegate: CPTPlotSpaceDelegate) {
-        var firstPoint: GraphPoint
-        var lastPoint: GraphPoint
-        
-        if (points.count > 0) {
-            firstPoint = points[points.count - 1]
-            lastPoint = points[0]
-        } else {
-            firstPoint = GraphPoint(x: 0, y: 0)
-            lastPoint = GraphPoint(x: 0, y: 0)
-        }
+
         let tickInterval = 10.0
+        let visiblePoints = 12
+        let defaultMarginX: Double = 2 * 86400 // 2 days
         
+        // Guarantee that this array has atleast one point for preceding calculations
+        let configurationPoints = points.count == 0 ? [GraphPoint(x: 0, y: 0)] : points
+        
+        let oldestPoint = configurationPoints.last!
+        let mostRecentPoint = configurationPoints.first!
+
         let yRange = maxY - minY > 0 ? maxY - minY : tickInterval
         
-        var visibleMin = firstPoint
-        if (points.count > 12) {
-            visibleMin = points[12]
+        var visibleMin = oldestPoint
+        if (configurationPoints.count > visiblePoints) {
+            visibleMin = configurationPoints[visiblePoints]
         }
         
-        var marginX: Double = (lastPoint.x - visibleMin.x) * 0.1
+        var marginX: Double = (mostRecentPoint.x - visibleMin.x) * 0.1
         if (marginX == 0) {
-            marginX = 2 * 86400 // x days
+            marginX = defaultMarginX
         }
         
         var marginY: Double = tickInterval
-        let standardDeviationY = standardDeviation(points.flatMap{$0.y})
+        let standardDeviationY = standardDeviation(configurationPoints.flatMap{$0.y}) ?? 0.0
         marginY = max(marginY, standardDeviationY * 2)
         
-        self.xRange = CPTPlotRange(location_: visibleMin.x - marginX, length: lastPoint.x - visibleMin.x + marginX * 2)
+        self.xRange = CPTPlotRange(location_: visibleMin.x - marginX, length: mostRecentPoint.x - visibleMin.x + marginX * 2)
         self.yRange = CPTPlotRange(location_: minY - marginY, length: yRange + marginY * 2)
         
-        self.globalXRange = CPTPlotRange(location_: firstPoint.x - marginX, length: lastPoint.x - firstPoint.x + marginX * 2)
+        self.globalXRange = CPTPlotRange(location_: oldestPoint.x - marginX, length: mostRecentPoint.x - oldestPoint.x + marginX * 2)
         self.globalYRange = self.yRange
         
         self.delegate = delegate
