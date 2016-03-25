3c5b2d4a720a867c1c0d0699e0a6fe49346abd98
Remy Panicker <rpanicker@higi.com>
2016-03-25 17:30:48 -0500

    HC-182: Add API pagination model

---
+++ higi/Paging.swift
@@ -0,0 +1,14 @@
+//
+//  Paging.swift
+//  higi
+//
+//  Created by Remy Panicker on 3/25/16.
+//  Copyright © 2016 higi, LLC. All rights reserved.
+//
+
+import Foundation
+
+/// Class which represents pagination objects for fetching subsets of large collections while working with the higi API.
+final class Paging {
+    
+}
