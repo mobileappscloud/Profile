92913345cb9562715f6a6d57c604c55fd18d13a3
Remy Panicker <rpanicker@higi.com>
2016-07-21 11:05:50 -0500

    HMI-100: Remove unused code

--- higi/AppDelegate.swift
+++ higi/AppDelegate.swift
@@ -66,16 +66,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        SessionData.Instance.save();
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-        let refreshMinutes = 15.0
-        let refreshInterval: NSTimeInterval = 60.0 * refreshMinutes
-        if (SessionData.Instance.user != nil && NSDate().timeIntervalSinceDate(SessionData.Instance.lastUpdate) > refreshInterval) {
-            NSNotificationCenter.defaultCenter().postNotificationName("RefreshDashboard", object: nil)
-        }
     }
     
     // MARK: - Helper --> Should be refactored out of this class
