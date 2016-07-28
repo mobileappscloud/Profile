55b0cb710d5a548d7b0f546e6a41c7bcc5700170
Remy Panicker <rpanicker@higi.com>
2016-07-28 15:16:20 -0500

    HCT-627: Add Facebook app events

--- higi/AppDelegate.swift
+++ higi/AppDelegate.swift
@@ -58,6 +58,10 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return canHandleResource
     }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        FBSDKAppEvents.activateApp()
+    }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
